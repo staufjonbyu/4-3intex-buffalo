67acd39d5546e2d316b49b3bc12bf9cc93135a34
Jon Stauffer <staufjon@byu.edu>
2023-04-12 19:43:24 -0600

    cry

--- src/PageBody/Admin.js
+++ src/PageBody/Admin.js
@@ -1,61 +1,102 @@
-import { Authenticator, useAuthenticator, withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import { API, Auth } from 'aws-amplify';
-import React, {useState, useEffect} from 'react';
-
-import awsconfig from './../aws-exports';
-
-
-
-
-
+import { useEffect, useState } from 'react';
+import { API } from 'aws-amplify';
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Auth, Authenticator, AmplifySignOut } from 'aws-amplify';
 
 function Admin() {
-  const { authStatus } = useAuthenticator(context => [context.authStatus]);
-
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const [users, setUsers] = useState([]);
+  
+  // useEffect(() => {
+  //   async function getUsers() {
+  //     try {
+  //       const session = await Auth.currentSession();
+  //       const token = session.getAccessToken().getJwtToken();
+  //       const apiName = 'your-api-name'; // Replace with your API name
+  //       const path = '/users';
+  //       const headers = {
+  //         Authorization: token
+  //       };
+  //       const userData = await API.get(apiName, path, { headers });
+  //       setUsers(userData);
+  //     } catch (error) {
+  //       console.log(error);
+  //       setErrorMessage(error.message);
+  //     }
+  //   }
+  //   getUsers();
+  // }, []);
+  
 
-  useEffect(() => {
-    async function fetchUsers() {
-      try {
-        const usersData = await Auth.listUsers('us-east-1_0B3eMtrZ1');
-        setUsers(usersData);
-      } catch (error) {
-        console.log('Error listing users', error);
-      }
+
+  async function handleCreateUser(event) {
+    event.preventDefault();
+
+    try {
+      await Auth.signUp({
+        username,
+        password,
+        attributes: {
+          email,
+        },
+      });
+      setErrorMessage('');
+      setUsername('');
+      setPassword('');
+      setEmail('');
+      alert('User created successfully!');
+    } catch (error) {
+      setErrorMessage(error.message);
     }
+  }
 
-    fetchUsers();
-  }, []);
-  // Use the value of authStatus to decide which page to render
   return (
-    <>
-      {authStatus === 'configuring'}
-      {authStatus !== 'authenticated' ?
-        <div>
-          <h4>please sign in</h4>
-        </div> :
-        <Authenticator>
-          <div>
-            <h3>Welcom to the admin portal</h3>
-
-          </div>
-          <div>
-          <h2>User List</h2>
-          <ul>
-            {users.map(user => (
-              <li key={user.Username}>
-                <p>Username: {user.Username}</p>
-                <p>Email: {user.Attributes.find(attr => attr.Name === 'email').Value}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-        </Authenticator>
-
-      }
-    </>
+    <div>
+      <h1>Create a new user</h1>
+      {errorMessage && <p>{errorMessage}</p>}
+      <form onSubmit={handleCreateUser}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Create User</button>
+      </form>
+      {/* <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.username} ({user.email})</li>
+        ))}
+      </ul>
+    </div> */}
+    </div>
   );
-};
+}
 
 export default withAuthenticator(Admin);

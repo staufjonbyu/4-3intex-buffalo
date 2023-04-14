65c138a3b562e5cb964d55f9dd36d3fba6a3ba56
Joshua Allen <joshutallen99@gmail.com>
2023-04-14 08:31:41 -0600

    GDPR

--- src/PageBody/Gpdr.js
+++ src/PageBody/Gpdr.js
@@ -1,57 +1,129 @@
-import React from 'react';
+import React from "react";
 
- 
 function GDPR() {
-    return(
-        <>
-            <p>
-
-            GDPR Privacy Notice for Architect Records
-            
-            This privacy notice sets out how we collect, use, and protect your personal data in compliance with the General Data Protection Regulation (GDPR) as implemented in the UK. The data controller responsible for your personal data is [Insert Name of Company], registered in [Insert Country], with registered office at [Insert Address], and contact email [Insert Email Address].
-            
-            What personal data do we collect?
-            
-            We collect personal data that you provide to us directly, such as your name, address, phone number, and email address. We may also collect information about your professional qualifications, experience, and education if you are an architect.
-            
-            How do we use your personal data?
-            
-            We use your personal data to provide you with information about our services, to respond to your inquiries, to manage our relationship with you, and to comply with our legal obligations. If you are an architect, we may also use your personal data to evaluate your qualifications and suitability for job openings.
-            
-            We may share your personal data with third parties who provide services to us, such as IT providers, payment processors, or professional advisers. We will only disclose your personal data to third parties who have agreed to comply with GDPR and who need the information in order to provide their services to us.
-            
-            We do not sell, rent, or trade your personal data to third parties for marketing purposes.
-            
-            How do we protect your personal data?
-            
-            We take reasonable and appropriate measures to protect your personal data from loss, misuse, unauthorized access, disclosure, alteration, and destruction. We have implemented physical, technical, and organizational security measures to protect your personal data, including access controls, firewalls, and encryption.
-            
-            Your rights under GDPR
-            
-            Under GDPR, you have the following rights with respect to your personal data:
-            
-            Right to access: You have the right to access your personal data that we hold.
-            Right to rectification: You have the right to request that we correct any inaccurate or incomplete personal data.
-            Right to erasure: You have the right to request that we delete your personal data under certain circumstances.
-            Right to restrict processing: You have the right to request that we limit the processing of your personal data under certain circumstances.
-            Right to data portability: You have the right to request that we provide you with a copy of your personal data in a structured, commonly used, and machine-readable format.
-            Right to object: You have the right to object to the processing of your personal data under certain circumstances.
-            To exercise your rights under GDPR, please contact us using the contact details provided below.
-            
-            How long do we keep your personal data?
-            
-            We keep your personal data for as long as necessary to provide you with our services, to manage our relationship with you, and to comply with our legal obligations. If you are an architect, we may retain your personal data for a longer period of time in order to evaluate your qualifications and suitability for job openings.
-            
-            Changes to this privacy notice
-            
-            We may update this privacy notice from time to time to reflect changes in our data processing practices or legal obligations. We will notify you of any material changes by email or by posting a notice on our website.
-            
-            Contact us
-            
-            If you have any questions or concerns about our data processing practices or this privacy notice, or if you wish to exercise your rights under GDPR, please contact us at [Insert Contact Information].
-            </p>
-        </>
-    )
+  return (
+    <>
+      <div className="container">
+        <h1 className="p-3">GDPR Privacy Notice</h1>
+        <p>
+          GDPR Privacy Notice for Architect Records This privacy notice sets out
+          how we collect, use, and protect your personal data in compliance with
+          the General Data Protection Regulation (GDPR) as implemented in the
+          UK. The data controller responsible for your personal data is Brigham
+          Young University, registered in the United States of America, with
+          registered office at 555 N 555 E Unknown, UT 84606, and contact email
+          archaeology@byu.edu.
+        </p>
+        <p>
+          What personal data do we collect?{" "}
+          <ul>
+            <li>
+              We collect personal data that you provide to us directly, such as
+              your name, address, phone number, and email address. We may also
+              collect information about your professional qualifications,
+              experience, and education if you are an architect.
+            </li>
+          </ul>
+        </p>
+        <p>
+          How do we use your personal data?{" "}
+          <ul>
+            <li>
+              We use your personal data to provide you with information about
+              our services, to respond to your inquiries, to manage our
+              relationship with you, and to comply with our legal obligations.
+              If you are an architect, we may also use your personal data to
+              evaluate your qualifications and suitability for job openings.
+            </li>
+            <li>
+              We may share your personal data with third parties who provide
+              services to us, such as IT providers, payment processors, or
+              professional advisers.
+            </li>{" "}
+            <li>
+              We will only disclose your personal data to third parties who have
+              agreed to comply with GDPR and who need the information in order
+              to provide their services to us.
+            </li>{" "}
+            <li>
+              We do not sell, rent, or trade your personal data to third parties
+              for marketing purposes.
+            </li>
+          </ul>
+        </p>
+        <p>
+          How do we protect your personal data?
+          <ul>
+            <li>
+              We take reasonable and appropriate measures to protect your
+              personal data from loss, misuse, unauthorized access, disclosure,
+              alteration, and destruction. We have implemented physical,
+              technical, and organizational security measures to protect your
+              personal data, including access controls, firewalls, and
+              encryption.
+            </li>
+          </ul>
+        </p>
+        <p>
+          Your rights under GDPR Under GDPR, you have the following rights with
+          respect to your personal data:
+          <ul>
+            <li>
+              Right to access: You have the right to access your personal data
+              that we hold.
+            </li>{" "}
+            <li>
+              Right to rectification: You have the right to request that we
+              correct any inaccurate or incomplete personal data.
+            </li>{" "}
+            <li>
+              Right to erasure: You have the right to request that we delete
+              your personal data under certain circumstances.
+            </li>{" "}
+            <li>
+              Right to restrict processing: You have the right to request that
+              we limit the processing of your personal data under certain
+              circumstances.
+            </li>{" "}
+            <li>
+              Right to data portability: You have the right to request that we
+              provide you with a copy of your personal data in a structured,
+              commonly used, and machine-readable format.
+            </li>{" "}
+            <li>
+              Right to object: You have the right to object to the processing of
+              your personal data under certain circumstances. To exercise your
+              rights under GDPR, please contact us using the contact details
+              provided below.
+            </li>
+          </ul>
+        </p>
+        <p>
+          How long do we keep your personal data?
+          <ul>
+            <li>
+              We keep your personal data for as long as necessary to provide you
+              with our services, to manage our relationship with you, and to
+              comply with our legal obligations. If you are an architect, we may
+              retain your personal data for a longer period of time in order to
+              evaluate your qualifications and suitability for job openings.
+            </li>
+          </ul>
+        </p>
+        <p>
+          Changes to this privacy notice We may update this privacy notice from
+          time to time to reflect changes in our data processing practices or
+          legal obligations. We will notify you of any material changes by email
+          or by posting a notice on our website.
+        </p>
+        <p>
+          Contact us If you have any questions or concerns about our data
+          processing practices or this privacy notice, or if you wish to
+          exercise your rights under GDPR, please contact us at 480-555-5555.
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default GDPR;

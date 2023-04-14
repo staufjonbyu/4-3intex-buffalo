6005fafa952d2b22e9b1e7bb45a51b22e0030651
Joshua Allen <joshutallen99@gmail.com>
2023-04-14 01:28:50 -0600

    changes

--- src/PageBody/Burial.js
+++ src/PageBody/Burial.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
 import "./BurialStyles.css";
 import "bootstrap/dist/css/bootstrap.css";
-import axios from 'axios';
+import axios from "axios";
 
 function convertAge(age) {
   const ages = {
@@ -98,11 +98,12 @@ function Burial() {
   }, [page, ages, sex, wrappings, hairColors, areas, depth, length, bNum]);
   //
 
-  function axiosDelete(x)
-  {
+  function axiosDelete(x) {
     const url = `${mainUrl}/Crud/${x.burialnumber}/${x.area}/${x.eastwest}/${x.squareeastwest}/${x.northsouth}/${x.squarenorthsouth}`;
-    axios.delete(url).then(res => {console.log(res); window.location.reload();});
-    
+    axios.delete(url).then((res) => {
+      console.log(res);
+      window.location.reload();
+    });
   }
   return (
     <>
@@ -122,16 +123,15 @@ function Burial() {
         >
           Reset Filters
         </button>
-        <a 
+        <a
           href="/newentry"
           class="pagination-button"
-          style={{textDecoration: 'none'}}
-          >
-            
-            Add Record
+          style={{ textDecoration: "none" }}
+        >
+          Add Record
         </a>
       </div>
-      
+
       {authStatus !== "authenticated" ? (
         <center>
           <div>
@@ -291,7 +291,7 @@ function Burial() {
                         <tr>
                           <td>
                             <a
-                              style={{textDecoration: 'none'}}
+                              style={{ textDecoration: "none" }}
                               href={`/burial/${x.burialnumber}/${x.area}/${x.eastwest}/${x.squareeastwest}/${x.northsouth}/${x.squarenorthsouth}`}
                               className="btn btn-dark"
                             >
@@ -307,7 +307,7 @@ function Burial() {
                           <td>{x.length}</td>
                           <td>
                             <a
-                              style={{textDecoration: 'none'}}
+                              style={{ textDecoration: "none" }}
                               href={`/edit/${
                                 x.burialnumber ? x.burialnumber : ""
                               }/${x.area ? x.area : ""}/${
@@ -323,9 +323,16 @@ function Burial() {
                             </a>
                           </td>
                           <td>
-                          <a style={{textDecoration: 'none'}} href="#" onClick={() => {axiosDelete(x);}}>Delete</a>
+                            <a
+                              style={{ textDecoration: "none" }}
+                              href="#"
+                              onClick={() => {
+                                axiosDelete(x);
+                              }}
+                            >
+                              Delete
+                            </a>
                           </td>
-                         
                         </tr>
                       );
                     })
@@ -528,7 +535,6 @@ function Burial() {
                         {length ? "Unsort" : "Sort"}
                       </button>
                     </th>
-                    
                   </tr>
                 </thead>
                 <tbody>
@@ -538,7 +544,7 @@ function Burial() {
                         <tr>
                           <td>
                             <a
-                              style={{textDecoration: 'none'}}
+                              style={{ textDecoration: "none" }}
                               href={`/burial/${x.burialnumber}/${x.area}/${x.eastwest}/${x.squareeastwest}/${x.northsouth}/${x.squarenorthsouth}`}
                             >
                               {x.burialnumber}
--- src/PageBody/Unsupervised/Findings.js
+++ src/PageBody/Unsupervised/Findings.js
@@ -5,27 +5,30 @@ function Findings(props) {
     <>
       <div>
         <h2>Cluster Analysis Explained</h2>
-        <ul>
-          <li>
+        <ul className="p-3">
+          <li className="p-2">
             We see that as the burials got deeper that{" "}
             <strong>Linen Textiles</strong> was used more often that{" "}
             <strong>Wool Textiles</strong>. In the depth of 0-0.5 Wool is used
             about 75% of the time and Linen about 25%. In depth of 0.5-2, it
             flips. Linen is 75% and Wool is 25%.
           </li>
-          <li>
+          <li className="p-2">
             In further analysis, we also see that <strong>Females</strong> that
             were found in burials were the ones found with more colorful and
             decorative materials. In the clusters with mostly females, like 1
             and 2, we see materials like <strong>Purple Weft Threads</strong>,{" "}
             <strong>Decorative Thread</strong>, and <strong>Ribbons</strong>.
           </li>
-          <li>
-            In further analysis, we also see that <strong>Females</strong> that
-            were found in burials were the ones found with more colorful and
-            decorative materials. In the clusters with mostly females, like 1
-            and 2, we see materials like <strong>Purple Weft Threads</strong>,{" "}
-            <strong>Decorative Thread</strong>, and <strong>Ribbons</strong>.
+          <li className="p-3">
+            As we looked further into the specific of{" "}
+            <strong>Head Direction</strong>, we noticed that the most common
+            direction was West. But with Cluster 2, the main head direction we
+            see is <strong>East</strong>. This cluster we have{" "}
+            <strong>Brown and Red Hair Colors</strong>, had a varied depth, and
+            had a majority of <strong>Females</strong>. This could possibly mean
+            that women with colored hair might have had the majority of{" "}
+            <strong>East Head Direction</strong>.
           </li>
         </ul>
       </div>
